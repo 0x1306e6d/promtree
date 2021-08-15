dbd3409de056f3fc6d77fe4dcdc8c66628534ac4
Gihwan Kim <ghkim3221@gmail.com>
2021-08-15 15:57:12 +0900

    Add recoil

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { RecoilRoot } from "recoil";
+
 import App from "./App";
 
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root")
 );
