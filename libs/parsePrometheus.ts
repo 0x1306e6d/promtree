50a8350c335bf1a2dfb0280b1e393bd10abc417d
Gihwan Kim <ghkim3221@gmail.com>
2021-08-16 13:48:34 +0900

    Fix description is not parsed all

--- libs/parsePrometheus.ts
+++ libs/parsePrometheus.ts
@@ -8,42 +8,33 @@ export default function parsePrometheus(content: string): {
   let meters: { [name: string]: Meter } = {};
   // TODO(ghkim3221): Refactoring
   content.split("\n").forEach((line) => {
-    if (line.startsWith("#")) {
-      const components = line.split(" ");
-      if (components.length > 1) {
-        if (components[1] === "HELP") {
-          if (components.length > 2) {
-            const [_, __, name, description] = components;
-            if (name in meters) {
-              meters[name] = produce(meters[name], (draft) => {
-                draft.description = description;
-              });
-            } else {
-              meters[name] = {
-                name: name,
-                description: description,
-                type: "",
-                count: 0,
-              };
-            }
-          }
-        } else if (components[1] === "TYPE") {
-          if (components.length > 2) {
-            const [_, __, name, type] = components;
-            if (name in meters) {
-              meters[name] = produce(meters[name], (draft) => {
-                draft.type = type;
-              });
-            } else {
-              meters[name] = {
-                name: name,
-                description: "",
-                type: type,
-                count: 0,
-              };
-            }
-          }
-        }
+    if (line.startsWith("# HELP")) {
+      const { name, description } = parseHelpText(line);
+      if (name in meters) {
+        meters[name] = produce(meters[name], (draft) => {
+          draft.description = description;
+        });
+      } else {
+        meters[name] = {
+          name: name,
+          description: description,
+          type: "",
+          count: 0,
+        };
+      }
+    } else if (line.startsWith("# TYPE")) {
+      const { name, type } = parseTypeInformation(line);
+      if (name in meters) {
+        meters[name] = produce(meters[name], (draft) => {
+          draft.type = type;
+        });
+      } else {
+        meters[name] = {
+          name: name,
+          description: "",
+          type: type,
+          count: 0,
+        };
       }
     } else {
       const nameIndex = Math.min(line.indexOf("{"), line.indexOf(" "));
@@ -58,4 +49,20 @@ export default function parsePrometheus(content: string): {
     }
   });
   return meters;
+}
+
+function parseHelpText(line: string): { name: string; description: string } {
+  const remaining = line.substring(7); // `# HELP` + trailing whitespace.
+
+  const name = remaining.substring(0, remaining.indexOf(" "));
+  const description = remaining.substring(name.length + 1);
+  return { name, description };
+}
+
+function parseTypeInformation(line: string): { name: string; type: string } {
+  const remaining = line.substring(7); // `# TYPE` + trailing whiespace.
+
+  const name = remaining.substring(0, remaining.indexOf(" "));
+  const type = remaining.substring(name.length + 1);
+  return { name, type };
 }
